b42312c67afc619fef0fce5f1c35a8bd0ec484a4
Savio dos Santos <savio.dossantos@outlook.com>
2023-04-09 03:48:50 -0300

    first card on the road

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreditCardOneComponent } from './components/credit-card-one/credit-card-one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreditCardOneComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/credit-card-one/credit-card-one.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-credit-card-one',
+  templateUrl: './credit-card-one.component.html',
+  styleUrls: ['./credit-card-one.component.scss']
+})
+export class CreditCardOneComponent {
+
+}
