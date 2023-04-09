2ffb6dd5f2c57a4aa71e6105895e9d810be9e18e
Savio dos Santos <savio.dossantos@outlook.com>
2023-04-09 03:52:47 -0300

    first commit header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreditCardOneComponent } from './components/credit-card-one/credit-card-one.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreditCardOneComponent
+    CreditCardOneComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
